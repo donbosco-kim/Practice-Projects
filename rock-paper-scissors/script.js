ab0212bee0aa497158e00f999755b808b01fdca5
Donbosco Kim <leonbosco2@gmail.com>
2025-08-24 14:53:45 -0500

    Add getPlayerChoice function
    - use prompt to get a player choice and convert player choice to lowercase
    - check for valid choice and return the player choice if valid

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,9 +1,10 @@
-
-
+//function to get computer choice
 function getComputerChoice() {
+    //generate random number between 0-2
     let randomNumber = Math.floor(Math.random() * 3);
     let computerChoice = '';
 
+    //assign rock, paper or scissors based on random number
     if (randomNumber === 0) {
         computerChoice = 'rock';
     }else if (randomNumber === 1) {
@@ -12,4 +13,20 @@ function getComputerChoice() {
         computerChoice = 'scissors';
     }
     return computerChoice;
+}
+
+//Test the function
+console.log(getComputerChoice());
+
+//function to get player choice
+function getPlayerChoice() {
+    //prompt player for input
+    let playerChoice = prompt ('Enter rock, paper or scissors: ').toLowerCase();
+    //check if player input is valid
+    if (playerChoice !== 'rock' && playerChoice !== 'paper' && playerChoice !== 'scissors') {
+        console.log('Please enter either rock, paper or scissors');
+        return playerChoice;
+    }else {
+        return playerChoice;
+    }
 }
