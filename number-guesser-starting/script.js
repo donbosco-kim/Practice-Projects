8f96f7a8bf29d375ab40b35c11a082096478abab
Donbosco Kim <leonbosco2@gmail.com>
2025-08-30 18:11:45 -0500

    Add a generateTarget Function
    - generate random integer between 0 and 9
    - test console the function to make sure it works

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -3,4 +3,9 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
-
+const generateTarget = () => {
+    //return a random integer between 0 and 9
+    return Math.floor(Math.random() * 10)
+}
+//test generateTarget function
+console.log(generateTarget());
