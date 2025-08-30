775a023f28033bdb5bad995e35c3b3490ca93f3f
Donbosco Kim <leonbosco2@gmail.com>
2025-08-30 18:32:47 -0500

    Add compareGuessess function
    -takes three arguments
    - calculate the distance of each guess from the secret number
    - return true if human guess is closer or tied with computer

--- number-guesser-starting/script.js
+++ number-guesser-starting/script.js
@@ -8,4 +8,19 @@ const generateTarget = () => {
     return Math.floor(Math.random() * 10)
 }
 //test generateTarget function
-console.log(generateTarget());
+//console.log(generateTarget());
+
+function compareGuesses(humanGuess, computerGuess, secretTargetNumber){
+    //calculate the distance of each guess from the secret number
+    const humanDifference = Math.abs(humanGuess - secretTargetNumber);
+    const computerDifference = Math.abs(computerGuess - secretTargetNumber);
+
+    //if human is closer or tied with computer, human wins
+    if (humanDifference <= computerDifference){
+        return true;
+    }else {
+        return false;
+    }
+}
+//test compareGuesses function
+//console.log(compareGuesses(5, 5, 10)); -> return true
