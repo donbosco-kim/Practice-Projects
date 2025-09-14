e6ebdc9e66e23ffedfa432eec191ebe039189593
Donbosco Kim <leonbosco2@gmail.com>
2025-09-14 16:51:30 -0500

    Implemented deleteTask function and updated loadTask function
    - search for index of task in the tasks array and remove it if found using splice
    - added a html btn element that call deleteTask if it is click
    - append the delete btn element to the list

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -22,7 +22,19 @@ function loadTask(task){
     //set the text value as the content of the list element
     li.textContent = task;
 
-    //append it to the task list
+    //create delete button
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "❌";
+
+    //use the deleteTask function
+    deleteBtn.onclick = function(){
+        deleteTask(task, li);
+    }
+
+    //append the deleteBtn to the list <li> element
+    li.appendChild(deleteBtn);
+
+    //append the list to the task list <ul> element
     taskList.appendChild(li);
 }
 
@@ -52,11 +64,24 @@ function updateTask(){
 
 //function to delete a task
 
-function deleteTask(){
+function deleteTask(task, listItem){
+    //find the index of the task in the array
+    const index = tasks.indexOf(task)
 
+    //remove the task from the array if found
+    if (index !== -1) {
+        tasks.splice(index, 1);
+    }
+
+    //remove the list item from the DOM
+    listItem.remove();
+
+    //save updated tasks to localStorage
+    saveTask();
 }
 
 //function to save a task
 function saveTask() {
+    //convert the tasks array to a JSON string and store it under the 'tasks' key
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
